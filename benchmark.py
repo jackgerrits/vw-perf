5cea00e28377b7913c11fd4e3b357c2825b62a7e
Jack Gerrits <jackgerrits95@gmail.com>
2020-01-02 18:52:30 -0500

    Work on progress

---
+++ benchmark.py
@@ -0,0 +1,174 @@
+import os
+import time
+import subprocess
+import json
+import textwrap
+
+import util
+import find
+import clone
+
+from typing import Callable, Tuple, List, Union, TextIO, Optional, Any, Dict
+
+
+def make_command_line_test(
+        vw_bin: str, command_line: str) -> Tuple[Callable[[], None], str]:
+    def command_line_test():
+        util.check_result_throw(
+            subprocess.run((vw_bin + " " + command_line).split(),
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT))
+
+    return command_line_test, command_line
+
+
+def get_steps(vw_bin: str) -> List[Tuple[Callable[[], None], str]]:
+    """Add functions here that will run as part of the harness. They should return the time in
+    seconds it took to run it.
+    """
+    return [
+        make_command_line_test(vw_bin, "--no_stdin"),
+        make_command_line_test(
+            vw_bin, "-d ./data/rcv1/rcv1/rcv1.train.vw.gz -f r_temp"),
+        # This specific test must run after the previous test as it uses the produced model.
+        make_command_line_test(
+            vw_bin, "-d ./data/rcv1/rcv1/rcv1.test.vw.gz -t -i r_temp"),
+        make_command_line_test(
+            vw_bin,
+            "-t --dsjson -d ./data/cb_data/cb_data.dsjson --cb_explore_adf --ignore XA -q UB "
+            "--epsilon 0.2 -l 0.5 --cb_type mtr --power_t 0"),
+        make_command_line_test(
+            vw_bin,
+            "--dsjson -d ./data/cb_data/cb_data.dsjson --cb_explore_adf --ignore XA -q UB "
+            "--epsilon 0.2 -l 0.5 --cb_type mtr --power_t 0"),
+    ]
+
+
+def run_harness(vw_bin: str,
+                num_runs: int,
+                step_generator: Callable[[str], List[Tuple[Callable[[], None],
+                                                           str]]] = get_steps):
+    benchmarks = []
+    steps = step_generator(vw_bin)
+    for step, name in steps:
+        print(f"\trunning bench: '{name}'")
+
+        runs = []
+        # Average over number of runs
+        for _ in range(num_runs):
+            start_time = time.perf_counter()
+            # Run the step
+            step()
+            end_time = time.perf_counter()
+            runs.append(end_time - start_time)
+        benchmarks.append({"vw_bin": vw_bin, "name": name, "runs": runs})
+    return benchmarks
+
+
+class PerfInfo:
+    def __init__(self, json_data=Optional[Dict[str, Any]]):
+        if json_data is not None:
+            self.perf_data = json_data
+        else:
+            self.perf_data = {}
+
+    @staticmethod
+    def from_file(file_name):
+        with open('data.json') as f:
+            return PerfInfo(json.load(f))
+
+    @staticmethod
+    def from_string(json_string):
+        return PerfInfo(json.loads(json_string))
+
+    def ref_exists(self, commit_ref: str) -> bool:
+        return commit_ref in self.perf_data
+
+    def set_ref_info(self, commit_ref: str, author, title, date):
+        if not self.ref_exists(commit_ref):
+            self.perf_data[commit_ref] = {}
+
+        self.perf_data[commit_ref]["author"] = author
+        self.perf_data[commit_ref]["title"] = title
+        self.perf_data[commit_ref]["date"] = date
+
+    def remove_ref(self, commit_ref):
+        self.perf_data.pop(commit_ref, None)
+
+    def add_benchmark_data(self, commit_ref, benchmark_name, runs):
+        if "benchmarks" not in self.perf_data[commit_ref]:
+            self.perf_data[commit_ref]["benchmarks"] = {}
+
+        if benchmark_name not in self.perf_data[commit_ref]["benchmarks"]:
+            self.perf_data[commit_ref]["benchmarks"][benchmark_name] = {}
+
+        if "runs" not in self.perf_data[commit_ref]["benchmarks"][
+                benchmark_name]:
+            self.perf_data[commit_ref]["benchmarks"][benchmark_name][
+                "runs"] = []
+
+        self.perf_data[commit_ref]["benchmarks"][benchmark_name][
+            "name"] = benchmark_name
+        self.perf_data[commit_ref]["benchmarks"][benchmark_name][
+            "runs"].extend(runs)
+        self.perf_data[commit_ref]["benchmarks"][benchmark_name][
+            "average"] = sum(self.perf_data[commit_ref]["benchmarks"]
+                             [benchmark_name]["runs"]) / len(
+                                 self.perf_data[commit_ref]["benchmarks"]
+                                 [benchmark_name]["runs"])
+
+    def save_to_file(self, file_name: str):
+        with open(file_name, 'w') as f:
+            json.dump(self.perf_data, f)
+
+
+def run(commits, num, from_ref, to_ref, num_runs, skip_existing):
+    COMMITS_REPOS_DIR = "./clones/"
+    BIN_NAME = "vw"
+    commits_to_process = clone.resolve_args_to_commit_list(
+        commits, num, from_ref, to_ref)
+
+    commits_and_bins = []
+    for commit in commits_to_process:
+        commit_path = os.path.realpath(os.path.join(COMMITS_REPOS_DIR, commit))
+        if not os.path.exists(commit_path):
+            print(
+                "{commit} does not exist. Please checkout and build using `python3 run.py clone "
+                f"--commits {commit}`")
+        vw_bins = find.find_all(BIN_NAME, commit_path)
+        commits_and_bins.append((commit, vw_bins[0]))
+
+    if os.path.exists('data.json'):
+        perf_info = PerfInfo.from_file('data.json')
+    else:
+        perf_info = PerfInfo()
+
+    for ref, vw_bin in commits_and_bins:
+        print(f"Testing {ref}")
+
+        try:
+            if perf_info.ref_exists(ref) and skip_existing == True:
+                print(f"Skipping {ref} found - skipping")
+                continue
+
+            # Run harness
+            benchmarks = run_harness(os.path.realpath(vw_bin), num_runs,
+                                     get_steps)
+
+            # Save commit info
+            info = clone.get_commit_info(ref)
+            perf_info.set_ref_info(ref, info["author"], info["title"],
+                                   info["date"])
+
+            # Record benchmark info
+            for bnch in benchmarks:
+                perf_info.add_benchmark_data(ref, bnch["name"], bnch["runs"])
+
+            # Save as we go in case of quit/crash
+            perf_info.save_to_file("data.json")
+
+        except util.CommandFailed as e:
+            perf_info.remove_ref(ref)
+            print(f"Skipping {ref}, failed with: {e}")
+            continue
+    perf_info.save_to_file("data.json")
--- prepare.py
+++ prepare.py
@@ -8,51 +8,60 @@ import sys
 import requests
 import util
 
+
 def untar_file(tar_file, final_file):
-  util.ensure_dirs_exist(tar_file)
-  util.ensure_dirs_exist(final_file)
+    util.ensure_dirs_exist(tar_file)
+    util.ensure_dirs_exist(final_file)
 
-  unzip_dir = Path(final_file)
+    unzip_dir = Path(final_file)
 
-  # Check if untarred file already exists
-  if not unzip_dir.exists():
-    tar = tarfile.open(tar_file, 'r:gz')
-    tar.extractall(str(unzip_dir))
-    tar.close()
+    # Check if untarred file already exists
+    if not unzip_dir.exists():
+        tar = tarfile.open(tar_file, 'r:gz')
+        tar.extractall(str(unzip_dir))
+        tar.close()
+
 
 def download_with_progress(url, filename):
-  util.ensure_dirs_exist(filename)
+    util.ensure_dirs_exist(filename)
 
-  with open(filename, 'wb') as f:
-    response = requests.get(url, stream=True)
-    total = response.headers.get('content-length')
+    with open(filename, 'wb') as f:
+        response = requests.get(url, stream=True)
+        total = response.headers.get('content-length')
 
-    if total is None:
-      f.write(response.content)
-    else:
-      downloaded = 0
-      total = int(total)
-      for data in response.iter_content(chunk_size=max(int(total/1000), 1024*1024)):
-        downloaded += len(data)
-        f.write(data)
-        done = int(50*downloaded/total)
-        sys.stdout.write('\r[{}{}]'.format('█' * done, '.' * (50-done)))
-        sys.stdout.flush()
-  sys.stdout.write('\n')
+        if total is None:
+            f.write(response.content)
+        else:
+            downloaded = 0
+            total = int(total)
+            for data in response.iter_content(
+                    chunk_size=max(int(total / 1000), 1024 * 1024)):
+                downloaded += len(data)
+                f.write(data)
+                done = int(50 * downloaded / total)
+                sys.stdout.write('\r[{}{}]'.format('█' * done,
+                                                   '.' * (50 - done)))
+                sys.stdout.flush()
+    sys.stdout.write('\n')
+
 
 def download_and_untar(url, tar_name, final_name):
-  if os.path.exists(tar_name):
-    print ("{} already exists - skipping download".format(tar_name))
-  else:
-    print ("Downloading {} to {}...".format(url, tar_name))
-    download_with_progress(url, tar_name)
+    if os.path.exists(tar_name):
+        print("{} already exists - skipping download".format(tar_name))
+    else:
+        print("Downloading {} to {}...".format(url, tar_name))
+        download_with_progress(url, tar_name)
 
-  if os.path.exists(final_name):
-    print ("{} already exists - skipping extracting".format(final_name))
-  else:
-    print ("Extracting {} to {}...".format(tar_name, final_name))
-    untar_file(tar_name, final_name)
+    if os.path.exists(final_name):
+        print("{} already exists - skipping extracting".format(final_name))
+    else:
+        print("Extracting {} to {}...".format(tar_name, final_name))
+        untar_file(tar_name, final_name)
+
 
 def run():
-  download_and_untar("http://hunch.net/~vw/rcv1.tar.gz", "./tmp/rcv1.tar.gz", "./data/rcv1")
-  download_and_untar("https://cpsdevtesting.blob.core.windows.net/perf-data/cb_data.json.tar.gz?sp=r&st=2019-08-08T17:07:58Z&se=2050-08-09T01:07:58Z&spr=https&sv=2018-03-28&sig=Y9xgnrzemDmLdLfqa9xIPdIBWMESCvMswTNTry%2Bgwj8%3D&sr=b", "./tmp/cb_data.tar.gz", "./data/cb_data")
+    download_and_untar("http://hunch.net/~vw/rcv1.tar.gz", "./tmp/rcv1.tar.gz",
+                       "./data/rcv1")
+    download_and_untar(
+        "https://cpsdevtesting.blob.core.windows.net/perf-data/cb_data.json.tar.gz?sp=r&st=2019-08-08T17:07:58Z&se=2050-08-09T01:07:58Z&spr=https&sv=2018-03-28&sig=Y9xgnrzemDmLdLfqa9xIPdIBWMESCvMswTNTry%2Bgwj8%3D&sr=b",
+        "./tmp/cb_data.tar.gz", "./data/cb_data")
